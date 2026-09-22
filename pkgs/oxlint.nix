b459bd71f8d35e0555b2569b7e74f412dab73ebc
y-cg <159802291+y-cg@users.noreply.github.com>
2026-09-22 10:40:54 +0000

    chore(oxlint): update package

--- pkgs/oxlint.nix
+++ pkgs/oxlint.nix
@@ -17,25 +17,25 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "oxlint";
-  version = "1.70.0";
+  version = "1.85.0";
 
   src = fetchFromGitHub {
     owner = "oxc-project";
     repo = "oxc";
     tag = "oxlint_v${finalAttrs.version}";
-    hash = "sha256-+eP+Pawp2ZE5SqgOy9mReGn0CmXB0Ynm9aupbKLRjhk=";
+    hash = "sha256-VlIDDUJC+5YDLpfLr92zOm0vjPAL0j6PCiHaukBpsdk=";
   };
 
   cargoDeps = rustPlatform.fetchCargoVendor {
     inherit (finalAttrs) pname version src;
-    hash = "sha256-kdV7K1YkaiEKfTNpGUgRGAOAIzdgrupsG4Kg6ZBasrU=";
+    hash = "sha256-iQfRPmpqG8Jbunw1iLYiIhVqCn4H+koGJROcVY5wvA0=";
   };
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     pnpm = pnpm_10;
     fetcherVersion = 3;
-    hash = "sha256-njuEuAvM1DE24ueA4ZlyE/SpwdaKTWgXCV341miVoDI=";
+    hash = "sha256-cLZG9Rh3U6+TNenceOneVk+RBdwmfnxHpDQGA9ipMEE=";
   };
 
   dontUseCmakeConfigure = true;
